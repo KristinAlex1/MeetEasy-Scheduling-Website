9d87ce638dcc9998332c6ffc12f349f5cf17c0a2
KristinAlex1 <kristinalex2001@gmail.com>
2024-10-26 16:57:21 -0400

    new logo and starting user authentication

--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
 
 
 const Header = () => {
-  return <nav className = "mx-auto py-2 px-4 flex justify-left">
+  return <nav className = "mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
 
-        <Link href ={"/"} className = "flex items-center"></Link>
-        <Image src = "/logo.png" width = "200" height = "200" alt = "logo" className="h-auto w-auto"/>
+        <Link href ={"/"} className = "flex items-center">
+        <Image src = "/logo3.png" width = "150" height = "40" alt = " MeetEasy logo" className=" h-16 w-auto"/>
+        </Link>
 
+        <div className="flex items-center gap-4">
+          <Link href = "/events?create=true">
+            <Button className = "flex items-center gap-2">
+              <PenBox size={18}/> Create Event
+            </Button>
+          </Link>
+          <Button variant = "outline"> Login</Button>
+        </div>
     </nav>;
   
 };
