1f4c41e6cc8da6acffbb4102b00172a72d233eea
KristinAlex1 <kristinalex2001@gmail.com>
2024-11-19 01:31:48 -0500

    Added a card component to the dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,23 @@
+"use client";
+
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import AppLayout from "../meetings/layout";
+import { useUser } from "@clerk/nextjs";
 
 const Dashboard = () => {
-  return  (
-    
-  <AppLayout/>
+  const { isLoaded, user } = useUser();
 
-  )
-  
+  return (
+    <>
+      <AppLayout>
+        <Card>
+          <CardHeader>
+            <CardTitle>Welcome, {user?.firstName}</CardTitle>
+          </CardHeader>
+        </Card>
+      </AppLayout>
+    </>
+  );
 };
 
 export default Dashboard;
-
-
-
