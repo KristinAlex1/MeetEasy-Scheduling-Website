bae02c8f94b8c7b9ab55995fac60a9f175182f7a
KrisCodes <128437250+KristinAlex1@users.noreply.github.com>
2024-11-25 00:31:56 -0500

    Delete prisma directory

--- prisma/migrations/20241110143137_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdId` on the `Booking` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Booking" DROP COLUMN "createdId",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
